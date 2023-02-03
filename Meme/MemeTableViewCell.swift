c3cfa5e6a8f5b7acc6ec07df366086e8d5eb6622
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2023-02-04 00:59:50 +0300

    all done

---
+++ Meme/MemeTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeTableViewCell.swift
+//  Meme
+//
+//  Created by Nawaf Alotaibi on 04/02/2023.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var memeImg: UIImageView!
+    
+    @IBOutlet weak var memeLabel: UILabel!
+}
---
+++ Meme/TableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TableViewController.swift
+//  Meme
+//
+//  Created by Nawaf Alotaibi on 04/02/2023.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var memes: [Meme] {
+        return (UIApplication.shared.delegate as! AppDelegate).memes
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeTableViewCell", for: indexPath) as! MemeTableViewCell
+        let meme = memes[indexPath.row]
+        cell.memeImg.image = meme.memedImage
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let memeView = self.storyboard?.instantiateViewController(withIdentifier: "DetailMemeViewController") as! DetailMemeViewController
+        memeView.meme = self.memes[indexPath.row]
+        self.navigationController?.pushViewController(memeView, animated: true)
+    }
+
+}
